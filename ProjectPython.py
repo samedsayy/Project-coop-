ca597a5ff06888693f779086bbca4b3c4fc472b1
SamedSay <samedsays@gmail.com>
2025-03-12 14:34:27 +0100

    Update Main file

--- ProjectPython.py
+++ ProjectPython.py
@@ -1,1 +1,4 @@
-print("hello world")
+# This is main python file that we are going to do our project.
+# At the end of the project, this file needs to be sent to Brigthspace.
+
+print("Welcome to Main File")
