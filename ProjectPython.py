b3838aaedbd31eb17a9976cd2ea4ae44b76e67f4
SamedSay <samedsays@gmail.com>
2025-03-27 15:22:02 +0100

    Update ProjectPython.py

--- ProjectPython.py
+++ ProjectPython.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 from file_handler import FileHandler
 from product import Product, DiscountedProduct
 class Emarket:
@@ -69,74 +68,3 @@ class Emarket:
     def __save_data(self):
         data_to_save = {p.name: {"price": p.price, "amount": p.amount} for p in self.__products.values()}
         self.__file_handler.save_phones(data_to_save)
-=======
-import os 
-
-
-class FileHandler:
-    def __init__(self, filename):
-        self.filename = filename
-
-    def load_phones(self):
-        """Load products from the file and return them as a dictionary."""
-        products = {}
-        try:
-            with open(self.filename, "r") as file:
-                for line in file:
-                    name, price, amount = line.strip().split(", ")
-                    products[name] = {'price': float(price), 'amount': int(amount)}
-        except FileNotFoundError:
-            print(f"Error: The file '{self.filename}' was not found.")
-            print(f"Current working directory: {os.getcwd()}")
-        except Exception as e:
-            print(f"An error occurred: {e}")
-        return products
-
-
-class Emarket:
-    def __init__(self):
-        self.__my_products = {}
-
-    def add_product(self,name,info):
-        self.__my_products[name] = info
-
-    def get_products(self):
-        return self.__my_products
-
-
-class Emarketapplication:
-    def __init__(self):
-        self.__emarket = Emarket()
-        self.__filehandler = FileHandler("list.txt")
-
-        products = self.__filehandler.load_phones()
-        for name, info in products.items():
-            self.__emarket.add_product(name,info)
-
-
-
-    def help(self):
-        print("Commands: ")
-        print("0 Exit")
-        print("1 Show products")
-
-
-    def display_products():
-        #Display all products
-        for name, info in self.__emarket.get_products().items():
-            print(f"Product: {name}, Price: {info[0]}, Amount: {info[1]}")
-
-    def execute(self):
-        self.help()
-        while True:
-            print("")
-            command = input("Command: ")
-            if command == "0":
-                break
-            elif command =="1":
-                self.display_products()
-
-
-app = Emarketapplication()
-app.execute()
->>>>>>> Stashed changes
